d49c5bf04bb2553cad226d36da8c7a42f3e29e65
Piotr Głudkowski <piotr.gludkowski@villagetech.pl>
2023-01-30 00:21:34 +0100

    Added WTTweakActions enum flags

---
+++ Tweaks/WTTweakActions.psm1
@@ -0,0 +1,13 @@
+﻿#########################################
+# WinTweaker WTTweakActions enum        #
+# created at: 2023-01-29                #
+# author: Piotr Gludkowski, VillageTech #
+#########################################
+
+[Flags()] enum WTTweakActions {
+    Invalid = -1
+    None    =  0
+    Enable  =  1
+    Disable =  2
+    Remove  =  4
+}
