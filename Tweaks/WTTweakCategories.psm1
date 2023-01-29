44a6cba63334b629b23d70d97ac61e100a49c636
Piotr Głudkowski <piotr.gludkowski@villagetech.pl>
2023-01-30 00:22:22 +0100

    Added WTTweakCategories enum

---
+++ Tweaks/WTTweakCategories.psm1
@@ -0,0 +1,14 @@
+﻿#########################################
+# WinTweaker WTTweakCategories enum     #
+# created at: 2023-01-29                #
+# author: Piotr Gludkowski, VillageTech #
+#########################################
+
+enum WTTweakCategories {
+    Unknown
+    System
+    Privacy
+    Security
+    BingApp
+    WindowsApp
+}
