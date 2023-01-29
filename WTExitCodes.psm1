9027ed34209d3ebebb4b6d26d27dc47a41ea0326
Piotr Głudkowski <piotr.gludkowski@villagetech.pl>
2023-01-30 00:20:54 +0100

    Added WTExitCodes enum

---
+++ WTExitCodes.psm1
@@ -0,0 +1,13 @@
+﻿#########################################
+# WinTweaker WTExitCodes class          #
+# created at: 2023-01-29                #
+# author: Piotr Gludkowski, VillageTech #
+#########################################
+
+class WTExitCodes {
+    static [int]$Success                 = 0
+    static [int]$ArgsError               = 1
+    static [int]$InvalidRecipe           = 2
+    static [int]$InvalidOperation        = 3
+    static [int]$OperationNotImplemented = 4
+}
