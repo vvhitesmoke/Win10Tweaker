005660f4d9e6f4cde292938eaed13983e4dee4df
Piotr Głudkowski <piotr.gludkowski@villagetech.pl>
2023-01-30 14:00:47 +0100

    Added WTTweakerModes enum

---
+++ WTTweakerModes.psm1
@@ -0,0 +1,13 @@
+﻿#########################################
+# WinTweaker WTTweakerModes enum        #
+# created at: 2023-01-29                #
+# author: Piotr Gludkowski, VillageTech #
+#########################################
+
+enum WTTweakerModes {
+    Error
+    Tweak
+    Recipe
+    List
+    Info
+}
