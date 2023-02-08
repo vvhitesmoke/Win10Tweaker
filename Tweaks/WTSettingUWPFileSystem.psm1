6059e3ae4a1d8e32f5a68b9774ee78e73adf5006
Piotr Głudkowski <piotr.gludkowski@villagetech.pl>
2023-02-08 10:42:06 +0100

    Added tweaks: UWPFileSystem, UWPSwapFile

---
+++ Tweaks/WTSettingUWPFileSystem.psm1
@@ -0,0 +1,43 @@
+﻿###########################################
+# WinTweaker WTSettingUWPFileSystem class #
+# created at: 2023-02-08                  #
+# author: Piotr Gludkowski, VillageTech   #
+###########################################
+
+Using module .\WTTweakActions.psm1
+Using module .\WTTweakBase.psm1
+Using module .\WTTweakCategories.psm1
+
+class WTSettingUWPFileSystem : WTTweakBase {
+    WTSettingUWPFileSystem() {
+        $this.Name        = "SettingUWPFileSystem"
+        $this.Alias       = "UWPFileSystem"
+        $this.Description = "Access to libraries and file system from UWP apps."
+        $this.AllowedOperations = [WTTweakActions]::Enable +
+                                  [WTTweakActions]::Disable
+        $this.Categories        = [WTTweakCategories]::System,
+                                  [WTTweakCategories]::Security,
+                                  [WTTweakCategories]::Privacy,
+                                  [WTTweakCategories]::UWP
+    }
+
+    [bool]EnableTweak() {
+        # UWPFileSystem
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\documentsLibrary" -Name "Value" -Type String -Value "Allow"
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\picturesLibrary" -Name "Value" -Type String -Value "Allow"
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\videosLibrary" -Name "Value" -Type String -Value "Allow"
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\broadFileSystemAccess" -Name "Value" -Type String -Value "Allow"
+
+        return $true
+    }
+
+    [bool]DisableTweak() {
+        # UWPFileSystem
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\documentsLibrary" -Name "Value" -Type String -Value "Deny"
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\picturesLibrary" -Name "Value" -Type String -Value "Deny"
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\videosLibrary" -Name "Value" -Type String -Value "Deny"
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\broadFileSystemAccess" -Name "Value" -Type String -Value "Deny"
+
+        return $true
+    }
+}
---
+++ Tweaks/WTSettingUWPSwapFile.psm1
@@ -0,0 +1,40 @@
+﻿#########################################
+# WinTweaker WTSettingUWPSwapFile class #
+# created at: 2023-02-08                #
+# author: Piotr Gludkowski, VillageTech #
+#########################################
+
+Using module .\WTTweakActions.psm1
+Using module .\WTTweakBase.psm1
+Using module .\WTTweakCategories.psm1
+
+class WTSettingUWPSwapFile : WTTweakBase {
+    WTSettingUWPSwapFile() {
+        $this.Name        = "SettingUWPSwapFile"
+        $this.Alias       = "UWPSwapFile"
+        $this.Description = " Creation and use of swapfile.sys."
+        $this.AllowedOperations = [WTTweakActions]::Enable +
+                                  [WTTweakActions]::Disable
+        $this.Categories        = [WTTweakCategories]::System,
+                                  [WTTweakCategories]::Security,
+                                  [WTTweakCategories]::SpacePreservation,
+                                  [WTTweakCategories]::SSDProtection,
+                                  [WTTweakCategories]::Performance,
+                                  [WTTweakCategories]::Privacy,
+                                  [WTTweakCategories]::UWP
+    }
+
+    [bool]EnableTweak() {
+        # UWPSwapFile
+        Remove-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "SwapfileControl" -ErrorAction SilentlyContinue
+
+        return $true
+    }
+
+    [bool]DisableTweak() {
+        # UWPSwapFile
+        Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" -Name "SwapfileControl" -Type Dword -Value 0
+
+        return $true
+    }
+}
