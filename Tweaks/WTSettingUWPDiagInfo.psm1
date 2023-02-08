d230313767cf2061bff3e12b8690470ec01d8527
Piotr Głudkowski <piotr.gludkowski@villagetech.pl>
2023-02-08 02:23:13 +0100

    Added tweaks: UWPSyncWithOtherDevices, UWPDiagInfo

---
+++ Tweaks/WTSettingUWPDiagInfo.psm1
@@ -0,0 +1,38 @@
+﻿#########################################
+# WinTweaker WTSettingUWPDiagInfo class #
+# created at: 2023-02-08                #
+# author: Piotr Gludkowski, VillageTech #
+#########################################
+
+Using module .\WTTweakActions.psm1
+Using module .\WTTweakBase.psm1
+Using module .\WTTweakCategories.psm1
+
+class WTSettingUWPDiagInfo : WTTweakBase {
+    WTSettingUWPDiagInfo() {
+        $this.Name        = "SettingUWPSyncWithOtherDevices"
+        $this.Alias       = "UWPSyncWithOtherDevices"
+        $this.Description = "Access to diagnostic info from UWP apps."
+        $this.AllowedOperations = [WTTweakActions]::Enable +
+                                  [WTTweakActions]::Disable
+        $this.Categories        = [WTTweakCategories]::System,
+                                  [WTTweakCategories]::Security,
+                                  [WTTweakCategories]::Privacy,
+                                  [WTTweakCategories]::UWP
+    }
+
+    [bool]EnableTweak() {
+        # UWPDiagInfo
+        Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" -Name "LetAppsGetDiagnosticInfo" -ErrorAction SilentlyContinue
+
+        return $true
+    }
+
+    [bool]DisableTweak() {
+        # UWPDiagInfo
+        [WTTweakBase]::CreateRegistryHive("HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy")
+        Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" -Name "LetAppsGetDiagnosticInfo" -Type DWord -Value 2
+
+        return $true
+    }
+}
